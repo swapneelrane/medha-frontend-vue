b23cc3c60f0c8233395f0d2ee2ac55f00a9eb098
Swapneel Rane <swapneelrane@gmail.com>
2021-01-29 18:24:59 +0530

    rename "About" to "Students"

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,13 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/students",
+    name: "Students",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "students" */ "../views/Students.vue")
   }
 ];
 
---
+++ src/views/Students.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Students">
+    <h1>This is a Students page</h1>
+  </div>
+</template>
