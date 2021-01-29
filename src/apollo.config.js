375e47599caa13d2b9509c0fc8cdaffcc431b9df
Swapneel Rane <swapneelrane@gmail.com>
2021-01-29 18:10:04 +0530

    add tooling for Apollo VSCode Extension

---
+++ src/apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      name: "medha-cis-graphql",
+      url: "34.217.26.87:1337/graphql"
+    },
+    includes: ["src/**/*.vue", "src/**/*.js"]
+  }
+};
