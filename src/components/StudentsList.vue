30d8c77e2137c63b835c25617833c425f2f922bc
Swapneel Rane <swapneelrane@gmail.com>
2021-01-29 18:40:56 +0530

    add StudentList component

---
+++ src/components/StudentsList.vue
@@ -0,0 +1,29 @@
+<template> </template>
+
+<script>
+import gql from "graphql-tag";
+
+const query = gql`
+  query {
+    students {
+      id
+      first_name
+      last_name
+      email
+      phone
+    }
+  }
+`;
+
+export default {
+  apollo: {
+    rockets: {
+      query
+    }
+  }
+};
+
+console.log(query);
+</script>
+
+<style></style>
