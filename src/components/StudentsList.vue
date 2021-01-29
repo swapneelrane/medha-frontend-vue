e23619c09708da1fea647ef04500a3fc5dbc1b91
Swapneel Rane <swapneelrane@gmail.com>
2021-01-29 19:13:01 +0530

    query and display student names

--- src/components/StudentsList.vue
+++ src/components/StudentsList.vue
@@ -1,12 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li v-for="student in students" :key="student.id">
+        Name: {{ student.first_name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import gql from "graphql-tag";
 
 const query = gql`
-  query students {
+  query getStudents {
     students {
       id
       first_name
@@ -25,7 +31,8 @@ export default {
   }
 };
 
-console.log(this.apollo.students);
+// console.log(query);
+//console.log(this.$students);
 </script>
 
 <style></style>
--- src/views/Students.vue
+++ src/views/Students.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="Students">
     <h1>This is a Students page</h1>
+    <students-list />
   </div>
 </template>
+
+<script>
+import StudentsList from "../components/StudentsList.vue";
+
+export default {
+  components: {
+    StudentsList
+  }
+};
+</script>
