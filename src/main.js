e3ed0ff16f8351cb3f22a573496e467bf1b100dc
Swapneel Rane <swapneelrane@gmail.com>
2021-01-29 18:04:15 +0530

    update main.js with VueApollo & ApolloClient conf

--- src/main.js
+++ src/main.js
@@ -2,11 +2,24 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueApollo from "vue-apollo";
+import ApolloClient from "apollo-boost";
+
+const apolloClient = new ApolloClient({
+  uri: "34.217.26.87:1337/graphql"
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+Vue.use(VueApollo);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
