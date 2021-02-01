0daec8e6aaf58f81b255caea0a61a774ab74460e
Swapneel Rane <swapneelrane@gmail.com>
2021-02-01 16:33:35 +0530

    add vuetify

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import VueApollo from "vue-apollo";
 import ApolloClient from "apollo-boost";
+import vuetify from "./plugins/vuetify";
 
 const apolloClient = new ApolloClient({
   uri: "http://34.217.26.87:1337/graphql"
@@ -21,5 +22,6 @@ new Vue({
   router,
   store,
   apolloProvider,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
